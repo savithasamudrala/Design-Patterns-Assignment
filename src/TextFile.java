441e432fd27e9653833401abdcd37a765963a633
savithasamudrala <savithacsamudrala@gmail.com>
2024-11-14 10:19:18 -0600

    User Stories 1 and 2 completed and a bug on user story 3 where output format has html tags

--- src/TextFile.java
+++ src/TextFile.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class TextFile {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TextFile {
+
+    private ArrayList<String> fileContent;
+
+    public TextFile(ArrayList<String> data){
+        fileContent = data;
+    }
+
+    public void printPlainText(){
+       for(int i = 0; i < fileContent.size(); i++){
+           System.out.println(fileContent.get(i));
+       }
+    }
+
+    public void printHTML(){
+        System.out.println("<html dir=\"ltr\" lang=\"en\">");
+        System.out.println("<head>");
+        for(int i = 0; i < fileContent.size(); i++){
+            System.out.println("<text=" + fileContent.get(i) + ">");
+        }
+        System.out.println("</head>");
+        System.out.println("</html>");
+    }
+
+    public void printMarkdown(){
+        String[] options = {"**", "***", "<sub>", "~~","_"};
+        Random rng = new Random();
+        int number = rng.nextInt(options.length);
+        System.out.println("#" + fileContent.get(0));
+
+        for(int i = 1; i < fileContent.size(); i++){
+            String option = options[number];
+            System.out.println("<text=" + fileContent.get(i) + ">");
+        }
+        System.out.println("</head>");
+        System.out.println("</html>");
+    }
+
+
+
 }
