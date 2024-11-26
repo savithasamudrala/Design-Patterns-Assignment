4bdf1d00c71fd5efdde528ee926168c2af51eb6c
savithasamudrala <savithacsamudrala@gmail.com>
2024-11-26 17:10:25 -0600

    Remove redundant files from src directory

--- src/IFormat.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.util.ArrayList;
-
-public interface IFormat {
-
-    public void format(ArrayList<String> data);
-    void saveToFile(ArrayList<String> data, String filePath) throws IOException;
-
-}
